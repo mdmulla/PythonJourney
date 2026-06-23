99222a9d7530392814ded442d2f86bedde62862a
Ali <contactmoulaalinow@gmail.com>
2026-06-23 19:58:56 +0530

    Day 1: Basics of Python

---
+++ Practice/Day1/practice.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name:")
+age = int(input("Enter Your Age: "))
+city = input("Enter your City: ")
+
+print("===== DETAILS =====")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
