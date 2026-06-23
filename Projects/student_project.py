6d4dccf071257e91650c57a6407e9f1b9c4ce62d
Ali <contactmoulaalinow@gmail.com>
2026-06-23 20:00:35 +0530

    Student Profile Mini Project

---
+++ Projects/student_project.py
@@ -0,0 +1,17 @@
+print("=========================")
+print("    STUDENT PROFILE")
+print("=========================")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college: ")
+goal = input("Enter your goal: ")
+
+print("-------------------------")
+print(f"Name\t: {name}")
+print(f"Age\t: {age}")
+print(f"College\t: {college}")
+print(f"Goal\t: {goal}")
+print("-------------------------")
+
+print("Profile created sucessfully!")
